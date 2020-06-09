646b58dba13d104253f9e54b1d7323946f06b86b
Ismael <ismael9996@gmail.com>
2020-06-09 12:14:53 +0100

    Rover rotates left and right-Green

--- src/main/kotlin/Rover.kt
+++ src/main/kotlin/Rover.kt
@@ -11,4 +11,30 @@ data class Rover(var x: Int, var y: Int, var orientation: Char){
         }
 
     }
+
+    fun rotate(direction: Char){
+        var coordinates = "NESW"
+        var indexActualOrientation = coordinates.indexOf(this.orientation)
+
+        if(direction == 'r'){
+            if(indexActualOrientation == 3){
+                indexActualOrientation = 0
+            }
+            else {
+                indexActualOrientation++
+            }
+        }
+
+        if(direction == 'l'){
+            if(indexActualOrientation == 0){
+                indexActualOrientation = 3
+            }
+            else{
+                indexActualOrientation--
+            }
+        }
+
+        this.orientation = coordinates[indexActualOrientation]
+
+    }
 }
--- src/test/kotlin/RoverShould.kt
+++ src/test/kotlin/RoverShould.kt
@@ -7,7 +7,7 @@ class RoverShould{
     fun be_in_a_given_coordinates(){
         val axisX = 0
         val axisY = 0
-        val orientation = 'S'
+        val orientation = 'N'
 
         val rover = Rover(x = axisX, y = axisY, orientation = orientation)
         assertEquals(rover.x, axisX)
@@ -15,10 +15,10 @@ class RoverShould{
     }
 
     @Test
-    fun be_oriented(){
+    fun be_in_a_given_orientation(){
         val axisX = 0
         val axisY = 0
-        val orientation = 'S'
+        val orientation = 'N'
 
         val rover = Rover(x = axisX, y = axisY, orientation = orientation)
         assertEquals(rover.orientation, orientation)
@@ -28,7 +28,7 @@ class RoverShould{
     fun move_to_the_front(){
         val axisX = 0
         val axisY = 0
-        val orientation = 'S'
+        val orientation = 'N'
         val movement = 'f'
 
         val rover = Rover(x = axisX, y = axisY, orientation = orientation)
@@ -40,12 +40,30 @@ class RoverShould{
     fun move_to_the_back(){
         val axisX = 0
         val axisY = 0
-        val orientation = 'S'
+        val orientation = 'N'
         val movement = 'b'
 
         val rover = Rover(x = axisX, y = axisY, orientation = orientation)
         rover.move(movement)
         assertEquals(axisY - 1, rover.y)
+    }
+
+    @Test
+    fun be_reoriented(){
+        val axisX = 0
+        val axisY = 0
+        val orientation = 'N'
+        var movement = 'r'
+
+        var rover = Rover(x = axisX, y = axisY, orientation = orientation)
+        rover.rotate(movement)
+        assertEquals('E', rover.orientation)
+
+        movement = 'l'
+
+        rover = Rover(x = axisX, y = axisY, orientation = orientation)
+        rover.rotate(movement)
+        assertEquals('W', rover.orientation)
     }
 
 
