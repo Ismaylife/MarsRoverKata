d207163ab7da5390db158704679900dfe3e2f797
Ismael <ismael9996@gmail.com>
2020-06-10 13:05:31 +0100

    Rover stops if there is an obstacle - Green

--- src/main/kotlin/Rover.kt
+++ src/main/kotlin/Rover.kt
@@ -1,32 +1,34 @@
 data class Rover(var x: Int, var y: Int, var orientation: Char) {
 
-    val map = Map(10 , 10)
+    private val map = Map(10, 10)
+    private val obstacle = Obstacle(5, 5)
+    private val obstacleList = listOf<Obstacle>(obstacle)
 
     fun move(commands: String) {
         for (command: Char in commands) {
-            when(command) {
-                'f' -> goFront ()
-                'b' -> goBack ()
-                'l','r' -> rotate(command)
+            when (command) {
+                'f' -> goFront()
+                'b' -> goBack()
+                'l', 'r' -> rotate(command)
             }
         }
     }
 
-    private fun goFront(){
+    private fun goFront() {
         when (this.orientation) {
-            'N' -> if (isIntoMapLimits(this.x, this.y + 1))this.y++ else this.y = 0
-            'E' -> if (isIntoMapLimits(this.x + 1, this.y))this.x++ else this.x = 0
-            'S' -> if (isIntoMapLimits(this.x, this.y - 1)) this.y-- else this.y = map.maxHeight
-            'W' -> if (isIntoMapLimits(this.x - 1, this.y)) this.x-- else this.x = map.maxWidth
+            'N' -> if (checkMovement(this.x, this.y + 1)) this.y++ else this.y = 0
+            'E' -> if (checkMovement(this.x + 1, this.y)) this.x++ else this.x = 0
+            'S' -> if (checkMovement(this.x, this.y - 1)) this.y-- else this.y = map.maxHeight
+            'W' -> if (checkMovement(this.x - 1, this.y)) this.x-- else this.x = map.maxWidth
         }
     }
 
-    private fun goBack(){
+    private fun goBack() {
         when (this.orientation) {
-            'N' -> if (isIntoMapLimits(this.x, this.y - 1)) this.y-- else this.y = map.maxHeight
-            'E' -> if (isIntoMapLimits(this.x - 1, this.y)) this.x-- else this.x = map.maxWidth
-            'S' -> if (isIntoMapLimits(this.x, this.y + 1))this.y++ else this.y = 0
-            'W' -> if (isIntoMapLimits(this.x + 1, this.y))this.x++ else this.x = 0
+            'N' -> if (checkMovement(this.x, this.y - 1)) this.y-- else this.y = map.maxHeight
+            'E' -> if (checkMovement(this.x - 1, this.y)) this.x-- else this.x = map.maxWidth
+            'S' -> if (checkMovement(this.x, this.y + 1)) this.y++ else this.y = 0
+            'W' -> if (checkMovement(this.x + 1, this.y)) this.x++ else this.x = 0
         }
     }
 
@@ -36,7 +38,7 @@ data class Rover(var x: Int, var y: Int, var orientation: Char) {
         var indexActualOrientation = coordinates.indexOf(this.orientation)
         var indexOfNewOrientation = indexActualOrientation
 
-        when(orientation){
+        when (orientation) {
             'r' -> {
                 if (indexActualOrientation == 3) {
                     indexOfNewOrientation = 0
@@ -57,31 +59,35 @@ data class Rover(var x: Int, var y: Int, var orientation: Char) {
         this.orientation = coordinates[indexOfNewOrientation]
     }
 
-    private fun isIntoMapLimits(x : Int, y: Int) : Boolean {
+    private fun checkMovement(x: Int, y: Int): Boolean {
+        if (isIntoMapLimits(x, y)) {
+            if (!isAnyObstacle(x, y)) return true
+        }
+        return false
+
+    }
+
+
+    private fun isIntoMapLimits(x: Int, y: Int): Boolean {
         val map = Map(10, 10)
         return !(x > 10 || x < 0 || y > 10 || y < 0)
     }
 
-    private fun Checker(){
-
-
-
-        /* fun checkMovement () : Boolean{
-             checkMapLimits()
-             checkMapObstacles()
-             return false
-         }*/
-
-        /*fun checkMapObstacles(){
-
-        }*/
+    private fun isAnyObstacle(x: Int, y: Int): Boolean {
+        var thereIs = false
+        for (obstacle in obstacleList) {
+            if (obstacle.x == x && obstacle.y == y) {
+                thereIs = true
+                return thereIs
+            }
+        }
+        return thereIs
     }
+
 
 }
 
 
-data class Map (val maxWidth: Int, val maxHeight: Int)
-
-data class Obstacles(var obstacles: List<Obstacle>)
+data class Map(val maxWidth: Int, val maxHeight: Int)
 
 data class Obstacle(val x: Int, val y: Int)
